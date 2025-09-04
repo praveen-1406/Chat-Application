cef1cbc3b33525cd10fc80489a69c329e16c80a2
Praveen Kumar Prajapati <prajapatipraveen1406@gmail.com>
2025-09-05 01:34:50 +0530

    check-auth api

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -94,27 +94,36 @@ export const logout = (req, res) => {
         console.log("Error in logout controller:", error.message);
         res.status(500).json({ message: "Internal server Error" });
     }
-}   
+}
 
-export const updateProfile=async(req,res)=>{
-    
+export const updateProfile = async (req, res) => {
+
     try {
 
-        const {profilePic}=req.body;
-        const userId=req.user._id;
+        const { profilePic } = req.body;
+        const userId = req.user._id;
 
-        if(!profilePic){
-            return res.status(400).json({message:"Profile pic is required"});
+        if (!profilePic) {
+            return res.status(400).json({ message: "Profile pic is required" });
         }
 
-        const uploadResponse=await cloudinary.uploader.upload(profilePic);
+        const uploadResponse = await cloudinary.uploader.upload(profilePic);
 
-        const updatedUser=await User.findByIdAndUpdate(userId,{profilePic:uploadResponse.secure_url},{new:true});
+        const updatedUser = await User.findByIdAndUpdate(userId, { profilePic: uploadResponse.secure_url }, { new: true });
 
         res.status(200).json(updatedUser)
-        
+
     } catch (error) {
-        console.log("Error in Profile controller:",error);
-        res.status(500).json({message:"Internal server error"});
+        console.log("Error in Profile controller:", error);
+        res.status(500).json({ message: "Internal server error" })
+    }
+}
+
+export const checkAuth=(req,res)=>{
+    try {
+        res.status(200).json(req.user)
+    } catch (error) {
+        console.log("Error in checkAuth controller:",error.message)
+        res.status(500).json({message:"Internal server Error"})
     }
 }
