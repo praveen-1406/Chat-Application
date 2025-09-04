67837fb2e402acd1453209241198426f4d3d78e2
Praveen Kumar Prajapati <prajapatipraveen1406@gmail.com>
2025-09-04 19:54:17 +0530

    signup api

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,35 +1,52 @@
-import User from "../models/user.model";
+import { generateToken } from "../lib/utils.js";
+import User from "../models/user.model.js";
 import bcrypt from "bcryptjs";
 
-export const signup =async (req, res) => {
-    const {fullName,email,password,}=req.body;
+export const signup = async (req, res) => {
+    const { fullName, email, password, } = req.body;
     try {
-        // hash password
-        if(password.length<6){
-            return res.status(400).json({message:"Password must be at least 6 characters"});
+
+        if (!fullName || !email || !password) {
+            return res.status(400).json({ message: "All Fields are required" });
         }
 
-        const user=await User.findOne({email});
+        // hash password
+        if (password.length < 6) {
+            return res.status(400).json({ message: "Password must be at least 6 characters" });
+        }
 
-        if(user) return res.status(400).json({message:"Email already exists"});
+        const user = await User.findOne({ email });
 
-        const salt=await bcrypt.genSalt(10);
-        const hashedPassword=await bcrypt.hash(password,salt);
+        if (user) return res.status(400).json({ message: "Email already exists" });
 
-        const newUser=new User({
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
+
+        const newUser = new User({
             fullName,
             email,
-            password:hashedPassword,
+            password: hashedPassword,
         });
 
-        if(newUser){
+        if (newUser) {
             //generate jwt token
-        }else{
-            res.status(400).json({message:"Invalid user data"});
+            generateToken(newUser._id, res)
+            await newUser.save();
+
+            res.status(201).json({
+                _id: newUser._id,
+                fullName: newUser.fullName,
+                email: newUser.email,
+                profilePic: newUser.profilePic,
+            })
+
+        } else {
+            res.status(400).json({ message: "Invalid user data" });
         }
-        
+
     } catch (error) {
-        
+        console.log("Error in signup controller:", error.message);
+        res.status(500).json({ message: "Internal server Error" });
     }
 }
 
