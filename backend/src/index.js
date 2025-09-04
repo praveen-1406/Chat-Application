43e5b389c05d345df2802b16026f7e87d5076de5
Praveen Kumar Prajapati <prajapatipraveen1406@gmail.com>
2025-09-05 02:31:32 +0530

    messaging routes

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,7 +2,8 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 
-import authRoute from "./routes/auth.route.js";
+import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
 import { connectDB } from "./lib/db.js";
 
 dotenv.config();
@@ -15,7 +16,10 @@ app.use(express.json());
 
 app.use(cookieParser());
 
-app.use("/api/auth",authRoute);
+app.use("/api/auth",authRoutes);
+app.use("/api/message",messageRoutes)
+
+
 
 app.listen(PORT,()=>{
     console.log(`Server is running: http://localhost:${PORT}`);
