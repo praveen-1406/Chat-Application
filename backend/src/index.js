8db9270b7482462a496c3911e33ce7a2c89c10f4
Praveen Kumar Prajapati <prajapatipraveen1406@gmail.com>
2025-09-04 21:24:40 +0530

    cloudinary setup

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+
 import authRoute from "./routes/auth.route.js";
 import { connectDB } from "./lib/db.js";
 
@@ -10,6 +12,8 @@ const app=express();
 const PORT=process.env.PORT;
 
 app.use(express.json());
+
+app.use(cookieParser());
 
 app.use("/api/auth",authRoute);
 
--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,5 +1,6 @@
 import express from "express";
-import { login, logout, signup } from "../controllers/auth.controller.js";
+import { login, logout, signup, updateProfile } from "../controllers/auth.controller.js";
+import { protectRoute } from "../middleware/auth.middleware.js";
 
 const router=express.Router();
 
@@ -9,6 +10,6 @@ router.post("/login",login)
 
 router.post("/logout",logout)
 
-router.put("/update-profile",updateProfile)
+router.put("/update-profile",protectRoute,updateProfile)
 
 export default router;
