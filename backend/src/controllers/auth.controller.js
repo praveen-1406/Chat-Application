8b791bc32baaf3f2ccda436a7d793912d61ecff4
Praveen Kumar Prajapati <prajapatipraveen1406@gmail.com>
2025-09-04 21:37:30 +0530

    update-profile api

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,3 +1,4 @@
+import cloudinary from "../lib/cloudinary.js";
 import { generateToken } from "../lib/utils.js";
 import User from "../models/user.model.js";
 import bcrypt from "bcryptjs";
@@ -96,10 +97,24 @@ export const logout = (req, res) => {
 }   
 
 export const updateProfile=async(req,res)=>{
-    const 
+    
     try {
+
+        const {profilePic}=req.body;
+        const userId=req.user._id;
+
+        if(!profilePic){
+            return res.status(400).json({message:"Profile pic is required"});
+        }
+
+        const uploadResponse=await cloudinary.uploader.upload(profilePic);
+
+        const updatedUser=await User.findByIdAndUpdate(userId,{profilePic:uploadResponse.secure_url},{new:true});
+
+        res.status(200).json(updatedUser)
         
     } catch (error) {
-        
+        console.log("Error in Profile controller:",error);
+        res.status(500).json({message:"Internal server error"});
     }
 }
