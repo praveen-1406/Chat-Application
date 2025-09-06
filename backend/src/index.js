0346680a20ecc2566d255765f96fa1e5d8abb301
Praveen Kumar Prajapati <prajapatipraveen1406@gmail.com>
2025-09-06 16:39:42 +0530

    signup and login pages

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,19 +4,21 @@ import cookieParser from "cookie-parser";
 
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
+import cors from "cors";
 import { connectDB } from "./lib/db.js";
 
 dotenv.config();
-
 const app=express();
-
 const PORT=process.env.PORT;
-
 app.use(express.json());
-
 app.use(cookieParser());
+app.use(cors({
+    origin:"http://localhost:5173",
+    credentials:true,
+}));
 
 app.use("/api/auth",authRoutes);
+
 app.use("/api/message",messageRoutes)
 
 
