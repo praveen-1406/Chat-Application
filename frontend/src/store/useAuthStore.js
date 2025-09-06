4c9333d8618a6dc312026c2460c897a48e3f94b9
Praveen Kumar Prajapati <prajapatipraveen1406@gmail.com>
2025-09-06 19:27:25 +0530

    profile page

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -22,7 +22,7 @@ export const useAuthStore=create((set)=>({
     signUp:async(data)=>{
         set({isSigningUp:true});
         try {
-            const res=await axiosInstance.post("auth/signup",data);
+            const res=await axiosInstance.post("/auth/signup",data);
             set({authUser:res.data})
             toast.success("Account created successfully")
         } catch (error) {
@@ -31,6 +31,18 @@ export const useAuthStore=create((set)=>({
             set({isSigningUp:false})
         }
     },
+    login:async(data)=>{
+        set({isLoggingIn:true});
+        try {
+            const res=await axiosInstance.post("/auth/login",data)
+            set({authUser:res.data})
+            toast.success("Logged in successfully")
+        } catch (error) {
+            toast.error(error.response.data.message);
+        } finally{
+            set({isLoggingIn:false})
+        }
+    },   
     logout:async()=>{
         try {
             await axiosInstance.post("/auth/logout");
@@ -39,5 +51,18 @@ export const useAuthStore=create((set)=>({
         } catch (error) {
             toast.error(error.response.data.message);
         }
-    }
+    },
+    updateProfile:async(data)=>{
+        set({isUpdatingProfile:true})
+        try {
+            const res=await axiosInstance.put("/auth/update-profile",data)
+            set({authUser:res.data})
+            toast.success("Profile updated successfully")
+        } catch (error) {
+            console.log("error in update profile:",error)
+            toast.error(error.response.data.message)
+        } finally{
+            set({isUpdatingProfile:false})
+        }
+    },
 }))
