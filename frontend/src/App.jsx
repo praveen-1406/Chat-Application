5d074e4060d69f035d8a5c8e6df610baacaea651
Praveen Kumar Prajapati <prajapatipraveen1406@gmail.com>
2025-09-07 13:08:27 +0530

    sidebar component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,11 +7,14 @@ import SettingsPage from "./pages/SettingsPage";
 import ProfilePage from "./pages/ProfilePage";
 import { useAuthStore } from "./store/useAuthStore";
 import { useEffect } from "react";
-import {Loader} from "lucide-react";
-import {Toaster} from "react-hot-toast";
+import { Loader } from "lucide-react";
+import { Toaster } from "react-hot-toast";
+import { useThemeStore } from "./store/useThemeStore";
 
 const App = () => {
   const { authUser, checkAuth, isCheckingAuth } = useAuthStore();
+
+  const { theme, } = useThemeStore();
 
   //   React’s rules of hooks say:
   //  Put everything you reference inside useEffect into the dependency array.
@@ -21,35 +24,35 @@ const App = () => {
 
   console.log(authUser)
 
-  if(isCheckingAuth && !authUser) return (
+  if (isCheckingAuth && !authUser) return (
     <div className="flex items-center justify-center h-screen">
       <Loader className="size-10 animate-spin" />
     </div>
   )
 
   return (
-    <div>
+    <div data-theme={theme}>
       <Navbar />
       <Routes>
         <Route
           path="/"
-          element={(authUser)?<HomePage />:<Navigate to="/login"/>}
-        /> 
+          element={(authUser) ? <HomePage /> : <Navigate to="/login" />}
+        />
         <Route
           path="/signup"
-          element={(!authUser)?<SignUpPage />:<Navigate to="/"/>}
+          element={(!authUser) ? <SignUpPage /> : <Navigate to="/" />}
         />
         <Route
           path="/login"
-          element={(!authUser)?<LoginPage />:<Navigate to="/"/>}
+          element={(!authUser) ? <LoginPage /> : <Navigate to="/" />}
         />
         <Route
           path="/settings"
-          element={<SettingsPage/>}
+          element={<SettingsPage />}
         />
         <Route
           path="/profile"
-          element={(authUser)?<ProfilePage/>:<Navigate to="/login"/>}
+          element={(authUser) ? <ProfilePage /> : <Navigate to="/login" />}
         />
       </Routes>
 
