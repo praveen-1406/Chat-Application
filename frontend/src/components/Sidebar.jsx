866ce6467c9288e90717a29e2e6df7c67ce736d6
Praveen Kumar Prajapati <prajapatipraveen1406@gmail.com>
2025-09-08 12:39:08 +0530

    bug fix

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -75,7 +75,7 @@ const Sidebar = () => {
                     </button>
                 ))}
 
-                {users.length === 0 && (
+                {filteredUsers.length === 0 && (
                     <div className="text-center text-zinc-500 py-4">No online users</div>
                 )}
             </div>
